59b72d3e356ee70737d5cee2b7e2e9cb2c11b5a9
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-05-18 22:37:49 +0900

    feat(pattern): add validator for regex pattern

---
+++ validators/string/pattern.ts
@@ -0,0 +1,22 @@
+// Copyright 2023-latest Tomoki Miyauchi. All rights reserved. MIT license.
+// This module is browser compatible.
+
+import error from "../error.json" assert { type: "json" };
+import { format, ScalarValidator } from "../../utils.ts";
+
+export class PatternValidator extends ScalarValidator<string> {
+  constructor(public readonly pattern: RegExp) {
+    super();
+    super.expect(({ input }) =>
+      format(error.should_be_but, `match ${this}`, `"${input}"`)
+    );
+  }
+
+  is(input: string): boolean {
+    return this.pattern.test(input);
+  }
+
+  toString(): string {
+    return `pattern of \`${this.pattern}\``;
+  }
+}
