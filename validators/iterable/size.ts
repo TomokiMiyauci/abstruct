dccc945ffbdcc2df5398b4844f1709b25902c006
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-05-18 21:57:35 +0900

    feat(size): add validator for element size

---
+++ validators/iterable/size.ts
@@ -0,0 +1,23 @@
+// Copyright 2023-latest Tomoki Miyauchi. All rights reserved. MIT license.
+// This module is browser compatible.
+
+import { getSize } from "./utils.ts";
+import { format, ScalarValidator } from "../../utils.ts";
+import error from "../error.json" assert { type: "json" };
+
+export class SizeValidator extends ScalarValidator<Iterable<unknown>> {
+  constructor(public size: number) {
+    super();
+    super.expect(({ input }) =>
+      format(error.should_be_but, this, getSize(input))
+    );
+  }
+
+  override is(input: Iterable<unknown>): boolean {
+    return getSize(input) === this.size;
+  }
+
+  toString(): string {
+    return `size of ${this.size}`;
+  }
+}
