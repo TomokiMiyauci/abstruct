9d0b8e34855c870d5e5939e8de211bb22e42439b
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-05-23 16:15:53 +0900

    feat(operators): add and operator

---
+++ operators/and.ts
@@ -0,0 +1,34 @@
+// Copyright 2023-latest Tomoki Miyauchi. All rights reserved. MIT license.
+// This module is browser compatible.
+
+import { iter } from "../iter_utils.ts";
+import {
+  Assert,
+  AssertiveValidator,
+  Validation,
+  ValidationError,
+} from "../types.ts";
+
+export class AndValidator<In, In_ extends Via, Via extends In = In & In_>
+  implements AssertiveValidator<In, In & In_ & Via> {
+  declare [Assert.symbol]: In & In_ & Via;
+  validators: Validation[] = [];
+
+  constructor(left: Validation<In, Via>, right: Validation<Via, In_>);
+  constructor(...validators: Validation[]) {
+    this.validators = validators;
+  }
+
+  *validate(input: In): Iterable<ValidationError> {
+    for (const validator of this.validators) {
+      const iterable = validator.validate(input);
+      const result = iter(iterable);
+
+      if (result.done) continue;
+
+      yield result.value;
+      yield* iterable;
+      return;
+    }
+  }
+}
