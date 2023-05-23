57a5289541ac33e7ffbb3b626467009aae3facc2
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-05-23 15:26:22 +0900

    feat(mod): export public modules

---
+++ mod.ts
@@ -0,0 +1,26 @@
+// Copyright 2023-latest Tomoki Miyauchi. All rights reserved. MIT license.
+// This module is browser compatible.
+
+export { assert, is, validate, type ValidateOptions } from "./validation.ts";
+export {
+  bigint,
+  boolean,
+  count,
+  empty,
+  eq,
+  gt,
+  instance,
+  int,
+  item,
+  lt,
+  maxCount,
+  minCount,
+  nonEmpty,
+  nullish,
+  number,
+  object,
+  pattern,
+  positive,
+  string,
+  type,
+} from "./combinator.ts";
