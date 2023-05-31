70c12dab6581d52b6f6de75bea23fb23a0f6a0cc
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-05-31 23:54:54 +0900

    feat(deps): update deps version

--- _tools/meta.ts
+++ _tools/meta.ts
@@ -42,37 +42,37 @@ export const makeOptions = (version: string): BuildOptions => ({
   },
   packageManager: "pnpm",
   mappings: {
-    "https://deno.land/x/isx@1.3.1/iterable/is_empty.ts": {
+    "https://deno.land/x/isx@1.4.0/iterable/is_empty.ts": {
       name: "@miyauci/isx",
       version: "1.4.0",
       subPath: "iterable/is_empty.js",
     },
-    "https://deno.land/x/isx@1.3.1/iterable/is_not_empty.ts": {
+    "https://deno.land/x/isx@1.4.0/iterable/is_not_empty.ts": {
       name: "@miyauci/isx",
       version: "1.4.0",
       subPath: "iterable/is_not_empty.js",
     },
-    "https://deno.land/x/isx@1.3.1/iterable/is_single.ts": {
+    "https://deno.land/x/isx@1.4.0/iterable/is_single.ts": {
       name: "@miyauci/isx",
       version: "1.4.0",
       subPath: "iterable/is_single.js",
     },
-    "https://deno.land/x/isx@1.3.1/is_string.ts": {
+    "https://deno.land/x/isx@1.4.0/is_string.ts": {
       name: "@miyauci/isx",
       version: "1.4.0",
       subPath: "is_string.js",
     },
-    "https://deno.land/x/isx@1.3.1/is_bigint.ts": {
+    "https://deno.land/x/isx@1.4.0/is_bigint.ts": {
       name: "@miyauci/isx",
       version: "1.4.0",
       subPath: "is_bigint.js",
     },
-    "https://deno.land/x/isx@1.3.1/is_nullable.ts": {
+    "https://deno.land/x/isx@1.4.0/is_nullable.ts": {
       name: "@miyauci/isx",
       version: "1.4.0",
       subPath: "is_nullable.js",
     },
-    "https://deno.land/x/isx@1.3.1/date/is_valid_date.ts": {
+    "https://deno.land/x/isx@1.4.0/date/is_valid_date.ts": {
       name: "@miyauci/isx",
       version: "1.4.0",
       subPath: "date/is_valid_date.js",
--- deps.ts
+++ deps.ts
@@ -3,17 +3,17 @@
 
 // TODO(miyauci): This module be external
 
-export { isEmpty } from "https://deno.land/x/isx@1.3.1/iterable/is_empty.ts";
-export { isNotEmpty } from "https://deno.land/x/isx@1.3.1/iterable/is_not_empty.ts";
-export { isSingle } from "https://deno.land/x/isx@1.3.1/iterable/is_single.ts";
-export { isNullable } from "https://deno.land/x/isx@1.3.1/is_nullable.ts";
-export { isString } from "https://deno.land/x/isx@1.3.1/is_string.ts";
-export { isBigint } from "https://deno.land/x/isx@1.3.1/is_bigint.ts";
+export { isEmpty } from "https://deno.land/x/isx@1.4.0/iterable/is_empty.ts";
+export { isNotEmpty } from "https://deno.land/x/isx@1.4.0/iterable/is_not_empty.ts";
+export { isSingle } from "https://deno.land/x/isx@1.4.0/iterable/is_single.ts";
+export { isNullable } from "https://deno.land/x/isx@1.4.0/is_nullable.ts";
+export { isString } from "https://deno.land/x/isx@1.4.0/is_string.ts";
+export { isBigint } from "https://deno.land/x/isx@1.4.0/is_bigint.ts";
 export { isPositiveNumber } from "https://deno.land/x/isx@1.4.0/numeric/is_positive_number.ts";
 export { isNonNegativeNumber } from "https://deno.land/x/isx@1.4.0/numeric/is_non_negative_number.ts";
 export { isNegativeNumber } from "https://deno.land/x/isx@1.4.0/numeric/is_negative_number.ts";
 export { isNonPositiveNumber } from "https://deno.land/x/isx@1.4.0/numeric/is_non_positive_number.ts";
-export { isValidDate } from "https://deno.land/x/isx@1.3.1/date/is_valid_date.ts";
+export { isValidDate } from "https://deno.land/x/isx@1.4.0/date/is_valid_date.ts";
 export { filterKeys } from "https://deno.land/std@0.187.0/collections/filter_keys.ts";
 export { maxBy } from "https://deno.land/std@0.187.0/collections/max_by.ts";
 import { default as escapeStringRegex } from "https://esm.sh/escape-string-regexp@5.0.0?pin=v122";
