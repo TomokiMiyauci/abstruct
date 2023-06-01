981baf4135f52e28fbb5bae2d69f0901ac2c9603
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-06-01 22:06:05 +0900

    test(item): add test case

--- validators/iterable/item_test.ts
+++ validators/iterable/item_test.ts
@@ -2,9 +2,20 @@
 
 import { ItemValidator } from "./item.ts";
 import { TypeValidator } from "../operators/typeof.ts";
+import { ValidationFailure } from "../../types.ts";
 import { assertEquals, describe, it } from "../../_dev_deps.ts";
 
 describe("ItemValidator", () => {
+  it("validate should yield", () => {
+    const validator = new ItemValidator(new TypeValidator("string"));
+
+    assertEquals([...validator.validate([0, false, 1n])], [
+      new ValidationFailure("", { instancePath: [0] }),
+      new ValidationFailure("", { instancePath: [1] }),
+      new ValidationFailure("", { instancePath: [2] }),
+    ]);
+  });
+
   it("should represent of", () => {
     assertEquals(
       new ItemValidator(new TypeValidator("string")).toString(),
