9fa6d0beb00d4edfb062c24ab7e01e97be2933dd
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-06-01 23:50:30 +0900

    docs(types): add description

--- types.ts
+++ types.ts
@@ -27,11 +27,17 @@ export class ValidationFailure {
   }
 }
 
+/** Validation failure options. */
 export interface ValidationFailureOptions {
+  /** The path to a part of the instance. */
   instancePath?: readonly PropertyKey[];
 }
 
+/** Binding message API. */
 export interface Expectation<T = unknown> {
+  /** Reserve message to be.  */
   expect(message: string): this;
+
+  /** Reserve dynamic message to be.  */
   expect(report: (context: T) => string): this;
 }
